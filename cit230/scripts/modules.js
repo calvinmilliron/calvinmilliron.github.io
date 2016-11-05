98ba0434193daf0e16403e2e85896776c587a468
Calvin <calvin.milliron@gmail.com>
2016-11-05 14:23:12 -0600

    modular large

--- cit230/scripts/modules.js
+++ cit230/scripts/modules.js
@@ -1,4 +1,5 @@
 $(function() {
   $("#page-header").load("/cit230/modules/header.html");
+  $("#page-nav").load("/cit230/modules/nav.html");
   $("#page-footer").load("/cit230/modules/footer.html");
 });
