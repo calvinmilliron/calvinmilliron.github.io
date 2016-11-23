3a42834a4b6bd78648188cad513b99da88b270ce
Calvin <calvin.milliron@gmail.com>
2016-11-23 10:50:49 -0700

    move franklin

---
+++ cit230/homework/weather/scripts/modules.js
@@ -0,0 +1,5 @@
+$(function() {
+  $("#page-header").load("/cit230/homework/weather/modules/header.html");
+  $("#page-nav").load("/cit230/homework/weather/modules/nav.html");
+  $("#page-footer").load("/cit230/homework/weather/modules/footer.html");
+});
--- cit230/scripts/modules.js
+++
@@ -1,5 +0,0 @@
-$(function() {
-  $("#page-header").load("/cit230/homework/modules/header.html");
-  $("#page-nav").load("/cit230/homework/modules/nav.html");
-  $("#page-footer").load("/cit230/homework/modules/footer.html");
-});
